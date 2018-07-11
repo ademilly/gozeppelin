092ed09838f2fbde1d556f5881e0b4223138a05f
Aurélien Demilly <demilly.aurelien@gmail.com>
2018-07-11 14:57:12 +0200

    add support for getting/setting permissions on notebooks

--- zeppelinsrv/main.go
+++ zeppelinsrv/main.go
@@ -21,8 +21,10 @@ var rate time.Duration
 func usage(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf(`
 endpoints:
-  - list => list notebooks available on %s
-  - run  => run notebooks by IDs given as comma separated URL parameter notebookIDs
+  - list          => list notebooks available on %s
+  - run	          => run notebooks by IDs given as comma separated URL parameter notebookIDs
+  - getpermission => get permission for notebook notebookID
+  - setpermission => set permission for notebooks notebooksIDs, using JSON from POST request
     `, hostname)))
 }
 
@@ -96,6 +98,81 @@ func run(w http.ResponseWriter, r *http.Request) {
 	w.Write(b)
 }
 
+func getPermission(w http.ResponseWriter, r *http.Request) {
+	client, err := newClient(w, r)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not connect to %s: %v", hostname, err), http.StatusInternalServerError)
+		return
+	}
+
+	parameters := r.URL.Query()
+	notebookID := strings.Join(parameters["notebookID"], "")
+
+	if notebookID == "" {
+		w.Write([]byte("url parameter `notebookID` is missing\n"))
+		usage(w, r)
+		return
+	}
+
+	log.Printf("retrieving permission for notebook %s request from %s", notebookID, r.RemoteAddr)
+	res, err := client.GetNotePermission(notebookID)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not get permission of notebook %s: %v", notebookID, err), http.StatusPartialContent)
+		return
+	}
+
+	b, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not format response from %s: %v", hostname, err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(b)
+}
+
+func setPermission(w http.ResponseWriter, r *http.Request) {
+	client, err := newClient(w, r)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not connect to %s: %v", hostname, err), http.StatusInternalServerError)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	var data zeppelin.Permission
+	err = decoder.Decode(&data)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not decode input json: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	parameters := r.URL.Query()
+	notebookIDs := strings.Split(strings.Join(parameters["notebookIDs"], ""), ",")
+
+	if len(notebookIDs) == 0 {
+		w.Write([]byte("url parameter `notebookIDs` is missing\n"))
+		usage(w, r)
+		return
+	}
+
+	log.Printf("setting notebook %s permission request from %s", notebookIDs, r.RemoteAddr)
+	for _, notebookID := range notebookIDs {
+		res, err := client.SetNotePermission(notebookID, data)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("could not set permission on notebook %s: %v", notebookID, err), http.StatusPartialContent)
+			return
+		}
+
+		b, err := json.MarshalIndent(res, "", "  ")
+		if err != nil {
+			http.Error(w, fmt.Sprintf("could not format response from %s: %v", hostname, err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(b)
+	}
+}
+
 func init() {
 	blocked = false
 	rate = 10 * time.Minute
@@ -112,6 +189,8 @@ func main() {
 	handler.HandleFunc("/", usage)
 	handler.HandleFunc("/list", list)
 	handler.HandleFunc("/run", run)
+	handler.HandleFunc("/getpermission", getPermission)
+	handler.HandleFunc("/setpermission", setPermission)
 
 	srv := &http.Server{
 		Addr:        addr,
