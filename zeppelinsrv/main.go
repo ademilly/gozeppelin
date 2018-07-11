754f21ae7fb1664006c8c39dc2d0bbb23a63d810
Aurélien Demilly <demilly.aurelien@gmail.com>
2018-07-11 11:56:42 +0200

    add timeout handling

--- zeppelinsrv/main.go
+++ zeppelinsrv/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/ademilly/gozeppelin/zeppelin"
 )
@@ -14,6 +15,8 @@ import (
 var client zeppelin.Client
 var hostname string
 var port string
+var blocked bool
+var rate time.Duration
 
 func usage(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf(`
@@ -60,11 +63,22 @@ func run(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if blocked {
+		w.Write([]byte("computation still going on, try again later ;)"))
+		return
+	}
+
+	blocked = true
+	go func() {
+		time.AfterFunc(rate, func() {
+			blocked = false
+		})
+	}()
+
 	parameters := r.URL.Query()
 	notebookIDs := strings.Split(strings.Join(parameters["notebookIDs"], ""), ",")
 
-	log.Println(notebookIDs)
-
+	log.Printf("running %v", notebookIDs)
 	res, err := client.RunNotebooks(notebookIDs)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("could not run all notebooks: %v", err), http.StatusPartialContent)
@@ -79,20 +93,31 @@ func run(w http.ResponseWriter, r *http.Request) {
 	w.Write(b)
 }
 
+func init() {
+	blocked = false
+	rate = 10 * time.Minute
+}
+
 func main() {
 	flag.StringVar(&port, "port", "8080", "port number on which to serve")
 	flag.StringVar(&hostname, "hostname", "localhost", "zeppelin service hostname")
 	flag.Parse()
 
-	add := fmt.Sprintf("0.0.0.0:%s", port)
-	srv := http.NewServeMux()
+	addr := fmt.Sprintf("0.0.0.0:%s", port)
+	handler := http.NewServeMux()
 
-	srv.HandleFunc("/", usage)
-	srv.HandleFunc("/list", list)
-	srv.HandleFunc("/run", run)
+	handler.HandleFunc("/", usage)
+	handler.HandleFunc("/list", list)
+	handler.HandleFunc("/run", run)
 
-	log.Printf("serving on %s", add)
-	if err := http.ListenAndServe(add, srv); err != nil {
+	srv := &http.Server{
+		Addr:        addr,
+		ReadTimeout: 5 * time.Second,
+		Handler:     http.TimeoutHandler(handler, 10*time.Second, "server not available for now, try later ;)"),
+	}
+
+	log.Printf("serving on %s", addr)
+	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("server stopped: %v\n", err)
 	}
 }
